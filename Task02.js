1924aacd46d77f8857f01a1538df43d8126ccecc
Dido <100509456+Dido2002@users.noreply.github.com>
2023-09-29 10:20:47 +0300

    Add files via upload

---
+++ Task02.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Button} from 'react-bootstrap';
+
+export default function Task02(){
+    return(
+        <div className="Task02">
+            <Button variant="success">Button</Button>
+        </div>
+    )
+}
